35a4ab4f35a75c0b8c6860d15d88c93fd6461cef
SwapX <service@swapx.org>
2020-08-24 00:53:18 +0800

    add interfaces

---
+++ contracts/interfaces/ISwapXV1Factory.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.5.0;
+
+interface ISwapXV1Factory {
+    event PairCreated(address indexed token0, address indexed token1, address pair, address pToken, uint);
+
+    function feeTo() external view returns (address);
+
+    function feeToSetter() external view returns (address);
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+
+    function allPairs(uint) external view returns (address pair);
+
+    function allPairsLength() external view returns (uint);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair, address pToken);
+
+    function setFeeTo(address) external;
+
+    function setFeeToSetter(address) external;
+}
