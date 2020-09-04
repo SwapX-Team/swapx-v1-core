0ba180dbe6cf95459e4fff686cf4ed17355de8df
SwapX Tech <service@swapx.org>
2020-09-04 15:35:00 +0800

    add SWP

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# swapx-v1-core
-Core contracts for the SwapX V1
+# SwapX v1 Core
+
+
+# Local Development
+
+The following assumes the use of `node@>=10`.
+
+## Install Dependencies
+
+`yarn`
+
+## Compile Contracts
+
+`yarn compile`
+
+## Run Tests
+
+`yarn test`
--- contracts/SwapXV1Factory.sol
+++ contracts/SwapXV1Factory.sol
@@ -1,12 +1,12 @@
 pragma solidity =0.5.16;
 
 import './interfaces/ISwapXV1Factory.sol';
-import './SwapXV1Pair.sol';
 import './libraries/PairNamer.sol';
 import './token/SwapXToken.sol';
+import './SwapXV1Pair.sol';
 
 
-contract SwapXV1Factory is ISwapXV1Factory{
+contract SwapXV1Factory is ISwapXV1Factory {
     address public feeTo;
     address public setter;
     address public miner;
@@ -20,6 +20,7 @@ contract SwapXV1Factory is ISwapXV1Factory{
     event PairCreated(address indexed token0, address indexed token1, address pair, address indexed ptoken, uint);
 
     constructor(address _setter) public {
+        require(_setter != address(0), 'SwapxV1: SETTER INVALID');
         setter = _setter;
     }
 
@@ -29,7 +30,6 @@ contract SwapXV1Factory is ISwapXV1Factory{
 
     function createPair(address tokenA, address tokenB) external returns (address pair, address pToken) {
         require(miner != address(0), 'SwapxV1: MINER INVALID');
-        //        require(pToken != address(0), 'SwapxV1: pToken INVALID');
         require(tokenA != tokenB, 'SwapxV1: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'SwapxV1: ZERO_ADDRESS');
@@ -47,7 +47,7 @@ contract SwapXV1Factory is ISwapXV1Factory{
         ISwapXV1Pair(pair).initialize(token0, token1);
         ISwapXToken(pToken).initialize("Pair Token", PairNamer.pairPtSymbol(tokenA, tokenB, "X"), 5760000 * 10 ** 18);
         ISwapXToken(pToken).addIssuer(miner);
-        ISwapXToken(pToken).transferOwnership(msg.sender);
+        ISwapXToken(pToken).transferOwnership(setter);
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair;
         token2Pair[pToken] = pair;
@@ -59,7 +59,7 @@ contract SwapXV1Factory is ISwapXV1Factory{
     }
 
     function setMiner(address _miner) external {
-        require(isContract(_miner),"SwapXV1: miner must be contract");
+        require(isContract(_miner), "SwapXV1: MINER MUST BE CONTRACT");
         require(msg.sender == setter, 'SwapXV1: FORBIDDEN');
         miner = _miner;
     }
@@ -71,13 +71,12 @@ contract SwapXV1Factory is ISwapXV1Factory{
 
 
     function setSetter(address _setter) external {
-        require(msg.sender == setter, 'SwapXV1: FORBIDDEN');
-        setter = _setter;
+        require(false,"SwapXV1: NOT SUPPORT");
     }
 
     function isContract(address addr) internal view returns (bool) {
         uint size;
-        assembly { size := extcodesize(addr) }
+        assembly {size := extcodesize(addr)}
         return size > 0;
     }
 }
